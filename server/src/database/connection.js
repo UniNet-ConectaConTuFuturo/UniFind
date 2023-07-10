b09d0d540dae16199c3ace91c01cba5a68eac9f9
Juerte99 <86316267+Juerte99@users.noreply.github.com>
2023-07-10 02:14:43 -0300

    .

--- server/src/database/connection.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2";
-
-export default mysql.createPool({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "uninet",
-});
--- server/src/index.js
+++
@@ -1,8 +0,0 @@
-//Modules
-import app from "./app.js";
-//CreateServer
-app.listen(app.get("port"), () => {
-  console.log(
-    `Servidor ${app.get("appName")} en el puerto ${app.get("port")} `
-  );
-});
