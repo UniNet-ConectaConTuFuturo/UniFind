98826f094c004d1044475557e737481da47fdb73
Juerte99 <86316267+Juerte99@users.noreply.github.com>
2023-07-10 14:33:31 -0300

    Base Layout

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,13 @@
+
+function Layout({children}) {
+  return (
+        <>
+        <div>
+            <aside>Hola</aside>
+        </div>
+        <main>{children}</main>
+        </>
+  )
+}
+
+export default Layout
