95eb6437001e2a650ce6ba19b95032652ddd79d5
Juerte99 <86316267+Juerte99@users.noreply.github.com>
2023-08-01 18:06:54 -0300

    un fix mas

--- client/src/api/markers/filter.js
+++ client/src/api/markers/filter.js
@@ -4,7 +4,7 @@ export default function setMarkersWithFilters(setMarkers, nombres, gestion) {
   setMarkers(
     json.filter(
       (u) =>
-      nombres.length > 0 ? (nombres.some((n) => n === u.nombre_universidad)) : true
+      (nombres.length > 0 ? (nombres.some((n) => n === u.nombre_universidad)) : true)
          &&
         (gestion === "0" ? true : gestion === u.gestion_universidad)
     )
