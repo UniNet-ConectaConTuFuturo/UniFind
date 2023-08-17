c98f9144875929f00658979ef41d38fde7af13be
Juerte99 <86316267+Juerte99@users.noreply.github.com>
2023-08-17 00:27:53 -0300

    Update Info.jsx

--- client/src/components/Mapa/Info.jsx
+++ client/src/components/Mapa/Info.jsx
@@ -24,7 +24,7 @@ function Info() {
   return (
     <>
       {idUniToShowInfo !== 0 && (
-        <div className="flex flex-col fixed z-10 top-0 right-2 w-4/12 -mr-4 content-end h-screen m-0 bg-shape bg-no-repeat zIndex-1000">
+        <div className="text-black flex flex-col fixed z-10 top-0 right-2 w-4/12 -mr-4 content-end h-screen m-0 bg-shape bg-no-repeat zIndex-1000">
           <button
             className=""
             type="button"
@@ -32,8 +32,18 @@ function Info() {
           >
             Cerrar
           </button>
-          <div></div>
-          {idUniToShowInfo}
+          <p>{universidad.nombre_universidad}</p>
+          <p>{universidad.direccion_universidad}</p>
+          <p>{universidad.maps_universidad}</p>
+          <p>{universidad.localidad_universidad}</p>
+          <p>{universidad.web_universidad}</p>
+          <p>{universidad.gestion_universidad}</p>
+          <p>{universidad.zona_universidad}</p>
+          <p>{universidad.correo_universidad}</p>
+          <h2>Grados y Pregrados</h2>
+          {carreras.map((carrera, i) => {
+            return <p key={i}>{carrera.nombre_carrera}</p>;
+          })}
         </div>
       )}
     </>
