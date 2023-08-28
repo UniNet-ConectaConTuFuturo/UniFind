b02aa93742111d090f0cb05d1d8d094bbef0ae44
Juerte99 <86316267+Juerte99@users.noreply.github.com>
2023-08-27 22:20:35 -0300

    .

--- client/src/components/Layout/Aside.jsx
+++ client/src/components/Layout/Aside.jsx
@@ -9,7 +9,7 @@ class SideBarIcon extends React.Component {
     return (
       <div
         className="sidebar-icon relative flex  
-      w-16 h-16 mt-6 mb-2 mx-auto 
+      w-16 h-16 mt-6 mb-2
       shadow-lg bg-in_bg rounded-xl  hover:rounded-3xl transition-all duration-300 align-middle z-5"
       >
         {this.props.icon}
@@ -24,30 +24,30 @@ SideBarIcon.propTypes = {
 
 const SideBar = () => {
   return (
-    <div className="sidebar flex flex-col float-right fixed z-10 top-0 left-0 content-end h-screen w-32 m-0 bg-bg-sb_bg opacity-90">
+    <div className="sidebar flex flex-col pl-4  fixed z-10 top-0 left-0  h-screen w-24 m-0 bg-bg-sb_bg opacity-90">
       <Link to="/" className="aside-link">
-      <div className="flex items-center pl-8">
-        <SideBarIcon icon={<FaHome size="40" className="ml-3 mt-2.5" />} />
-      <p className="aside-p flex absolute ml-16 text-white pt-3 pl-3">HOME</p>
-      </div>
+        <div className="flex items-center">
+          <SideBarIcon icon={<FaHome size="40" className="ml-3 mt-2.5" />} />
+          <p className="aside-p">HOME</p>
+        </div>
       </Link>
       <Link to="/identificacion/ingresante" className="aside-link">
-      <div className="flex items-center pl-8">
-        <SideBarIcon icon={<FaUserAlt size="40" className="ml-3 mt-2.5" />} />
-      <p className="aside-p flex absolute ml-16 text-white pt-3 pl-3">LOGIN</p>
-      </div>
+        <div className="flex items-center">
+          <SideBarIcon icon={<FaUserAlt size="40" className="ml-3 mt-2.5" />} />
+          <p className="aside-p">LOGIN</p>
+        </div>
       </Link>
       <Link to="/listainteres">
-      <div className="flex items-center pl-8">
-        <SideBarIcon icon={<FaStar size="40" className="ml-3 mt-2.5" />} />
-      <p className="aside-p flex absolute ml-16 text-white pt-3 pl-3">FAVORITOS</p>
-      </div>
+        <div className="flex items-center">
+          <SideBarIcon icon={<FaStar size="40" className="ml-3 mt-2.5" />} />
+          <p className="aside-p">FAVORITOS</p>
+        </div>
       </Link>
       <Link to="/configuracion">
-      <div className="flex items-center pl-8">
-        <SideBarIcon icon={<FaCog size="40" className="ml-3 mt-2.5" />} />
-      <p className="aside-p flex absolute ml-16 text-white pt-3 pl-3">CONFIGURACIÓN</p>
-      </div>
+        <div className="flex items-center">
+          <SideBarIcon icon={<FaCog size="40" className="ml-3 mt-2.5" />} />
+          <p className="aside-p">CONFIGURACIÓN</p>
+        </div>
       </Link>
     </div>
   );
