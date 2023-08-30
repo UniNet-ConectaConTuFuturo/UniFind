4c3a001797c302116f3f1499eac130162660da5a
Tuken04 <valentino_camp@hotmail.com>
2023-08-30 20:40:38 -0300

    Update solicitudes.js

--- server/src/controllers/solicitudes.js
+++ server/src/controllers/solicitudes.js
@@ -5,22 +5,16 @@ import { jwtConfig } from "../config.js";
 import * as consult from "../database/consults.js";
 
 export async function uploadCarta(req,res) {
+    const {content, token, id_universidad} = req.body
     try{
-        const content = req.body.content;
-        try{
-            const userID = jwt.verify(token, jwtConfig.SECRET);
-            const fileName = `${userID.id}.txt`;
-            const filePath = path.join("uninet/server/cartas",fileName);
-            await fs.writeFile(filePath,content);
-            await consult.insertSolicitud(userID.id,fileName,id_universidad);
-        }
-        catch(error){
-            console.error('Error verifying token:', error);
-            res.status(401).json({ message: 'Invalid token' });
-        }  
+        const userID = jwt.verify(token, jwtConfig.SECRET);
+        const fileName = `${userID.id}.txt`;
+        const filePath = path.join("uninet/server/cartas",fileName);
+        await fs.writeFile(filePath,content);
+        await consult.insertSolicitud(userID.id,fileName,id_universidad);
     }
     catch(error){
-        console.error('Error:', error);
-        res.status(500).send('Error saving file.');
-    }
+        console.error('Error verifying token:', error);
+        res.status(401).json({ message: 'Invalid token' });
+    }  
 }
