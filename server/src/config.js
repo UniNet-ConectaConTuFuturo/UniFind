4e317cfa2bc9f0fa6b0315bfb21cb80d1bae4697
MichatNovato <109147964+Tuken04@users.noreply.github.com>
2023-09-14 16:35:18 -0300

    Create config.js

---
+++ server/src/config.js
@@ -0,0 +1,12 @@
+export const sess = {
+    secret: "Volter123",
+    cookie: {
+      maxAge: 1000 * 60 * 60, //1 hora
+    },
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+  };
+  export const jwtConfig = {
+    SECRET: "jwtVolter123",
+  };
