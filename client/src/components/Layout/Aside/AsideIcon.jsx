766c4bed0e0894fde570c97f5a9e4a9ec72898c8
Juerte99 <86316267+Juerte99@users.noreply.github.com>
2023-10-09 11:37:06 -0300

    tooltip aside en progreso

--- client/src/components/Layout/Aside/AsideIcon.jsx
+++ client/src/components/Layout/Aside/AsideIcon.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function AsideIcon({ url, Icon, texto, onClick }) {
-  return (
-    <Link
-      to={url}
-      onClick={onClick}
-      className="sidebar-icon relative flex justify-start items-center h-16 w-16 shadow-lg bg-in_bg rounded-xl hover:rounded-3xl transition-all duration-300 overflow-x-hidden"
-    >
-      <Icon size="40" className="mx-2.5" />
-      <p className="aside-p">{texto}</p>
-    </Link>
-  );
-}
+const AsideIcon = ({ url, Icon, texto, onClick }) => (
+  <Link
+    to={url}
+    onClick={onClick}
+    className="sidebar-icon relative flex justify-start items-center h-16 w-16 shadow-lg bg-in_bg rounded-xl hover:rounded-3xl transition-all duration-300 overflow-x-hidden"
+  >
+    <Icon size="40" className="mx-2.5" />
+    <p className="aside-p">{texto}</p>
+  </Link>
+);
+
 AsideIcon.propTypes = {
   url: PropTypes.string,
   Icon: PropTypes.func,
   texto: PropTypes.string,
+  onClick: PropTypes.func,
 };
 export default AsideIcon;
--- client/src/components/Layout/Aside/AsideIconLock.jsx
+++ client/src/components/Layout/Aside/AsideIconLock.jsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { FaLock } from "react-icons/fa";
-function AsideIconLock({ url, Icon, texto, condition }) {
-  return (
+import styled from "@emotion/styled";
+import { token } from "@atlaskit/tokens";
+import Tooltip, { TooltipPrimitive } from "@atlaskit/tooltip";
+const InlineDialog = styled(TooltipPrimitive)`
+  background: white;
+  border-radius: ${token("border.radius", "4px")};
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+  box-sizing: content-box; /* do not set this to border-box or it will break the overflow handling */
+  color: #333;
+  max-height: 300px;
+  max-width: 300px;
+  padding: ${token("space.100", "8px")} ${token("space.150", "12px")};
+`;
+const AsideIconLock = ({ url, Icon, texto, condition }) => (
+  <Tooltip component={InlineDialog} content="Debe iniciar sesión">
     <Link
       to={url}
-      title={condition ? "Debe iniciar sesión" : ""}
       className={
         (condition ? "disabled" : "") +
         " sidebar-icon relative flex justify-start items-center h-16 w-16 shadow-lg bg-in_bg rounded-xl hover:rounded-3xl transition-all duration-300 overflow-x-hidden"
@@ -17,8 +29,9 @@ function AsideIconLock({ url, Icon, texto, condition }) {
       </div>
       <p className="aside-p">{texto}</p>
     </Link>
-  );
-}
+  </Tooltip>
+);
+
 AsideIconLock.propTypes = {
   url: PropTypes.string,
   Icon: PropTypes.func,
