7135df23a359779984ce3a2705144743e76dfb79
MichatNovato <86316267+Juerte99@users.noreply.github.com>
2023-10-12 13:59:51 -0300

    chau yarn

--- client/src/components/Layout/Aside/AsideIconLock.jsx
+++ client/src/components/Layout/Aside/AsideIconLock.jsx
@@ -1,21 +1,19 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { FaLock } from "react-icons/fa";
-import styled from "@emotion/styled";
-import { token } from "@atlaskit/tokens";
-import Tooltip, { TooltipPrimitive } from "@atlaskit/tooltip";
+/*
 const InlineDialog = styled(TooltipPrimitive)`
   background: white;
   border-radius: ${token("border.radius", "4px")};
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-  box-sizing: content-box; /* do not set this to border-box or it will break the overflow handling */
+  box-sizing: content-box; // do not set this to border-box or it will break the overflow handling
   color: #333;
   max-height: 300px;
   max-width: 300px;
   padding: ${token("space.100", "8px")} ${token("space.150", "12px")};
-`;
+`; */
+{/* <Tooltip component={InlineDialog} content="Debe iniciar sesión"> */}
 const AsideIconLock = ({ url, Icon, texto, condition }) => (
-  <Tooltip component={InlineDialog} content="Debe iniciar sesión">
     <Link
       to={condition ? "" : url}
       className={
@@ -29,8 +27,8 @@ const AsideIconLock = ({ url, Icon, texto, condition }) => (
       </div>
       <p className="aside-p">{texto}</p>
     </Link>
-  </Tooltip>
 );
+/* </Tooltip> */
 
 AsideIconLock.propTypes = {
   url: PropTypes.string,
--- client/src/components/Mapa/OutMapComponents/Filters/Custom/MultiValueLabel.jsx
+++ client/src/components/Mapa/OutMapComponents/Filters/Custom/MultiValueLabel.jsx
@@ -1,28 +1,24 @@
-import styled from "@emotion/styled";
-import { token } from "@atlaskit/tokens";
-
 import PropTypes from "prop-types";
 import { components } from "react-select";
-import Tooltip, { TooltipPrimitive } from "@atlaskit/tooltip";
 
-const InlineDialog = styled(TooltipPrimitive)`
+/* const InlineDialog = styled(TooltipPrimitive)`
   background: white;
   border-radius: ${token("border.radius", "4px")};
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-  box-sizing: content-box; /* do not set this to border-box or it will break the overflow handling */
+  box-sizing: content-box; // do not set this to border-box or it will break the overflow handling
   color: #333;
   max-height: 300px;
   max-width: 300px;
   padding: ${token("space.100", "8px")} ${token("space.150", "12px")};
-`;
+`; */
 const MultiValueLabel = (props) => {
   return (
-    <Tooltip component={InlineDialog} content={props.data.title}>
-      <components.MultiValueLabel {...props}>
+    <components.MultiValueLabel {...props}>
         {props.data.selectedOption || props.data.label}
       </components.MultiValueLabel>
-    </Tooltip>
   );
+  {/* <Tooltip component={InlineDialog} content={props.data.title}> */}
+  /* </Tooltip> */
 };
 MultiValueLabel.propTypes = {
   data: PropTypes.object,
