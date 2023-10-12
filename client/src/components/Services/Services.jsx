309be1be7af2cd6e1fb57c15a5132e140e9c9a21
MichatNovato <91853441+juliiiiiiii@users.noreply.github.com>
2023-10-12 17:16:12 -0300

    services

--- client/src/components/Services/Services.jsx
+++ client/src/components/Services/Services.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 function Services() {
   return (
     <>
-        <div className='h-1/2 w-full pl-28'>
-            <img src="/images/estudiantes.jpg" alt="" />
+    <div className='flex flex-col w-full'>
+        <div className='h-1/2 w-full pl-28 absolute object-fill'>
+            <img className='w-full h-full absolute' src="/images/estudiantes.jpg" alt="" />
             <h1>Centro de Información</h1>
             <button className='bg-black w-40 h-8'>Ingresar</button>
         </div>
-        <div className='h-1/2 w-full bottom-0 pl-28'>
+        <div className='h-1/2 w-full pl-28 bottom-0 absolute'>
+        <img className='w-full h-full absolute' src="/images/examen.png" alt="" />
             Sistema de Admisión
         </div>
+    </div>
     </>
   )
 }
