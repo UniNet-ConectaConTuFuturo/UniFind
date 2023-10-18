84536d6f0c8ace5f04e681b9241af0213ef91808
Juerte99 <86316267+Juerte99@users.noreply.github.com>
2023-10-18 16:43:49 -0300

    "Ver en el mapa"

--- client/src/components/ListaInteres/Card.jsx
+++ client/src/components/ListaInteres/Card.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { get } from "../../api/api";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Card({
   setButtonPopUpVerMas,
@@ -9,60 +10,65 @@ function Card({
   setIdUniToShowInfo,
   id_universidad,
 }) {
-  const [universidad, setUniversidad] = useState({});
+  const [universidad, setUniversidad] = useState(null);
 
   useEffect(() => {
     (async () => {
-      setUniversidad(await get("/get/uni", { id_universidad }));
+        setUniversidad(await get("/get/uni", { id_universidad }));
     })();
   }, [id_universidad]);
 
   return (
     <>
-      <div className="card w-full h-full">
-        <div className="card-body">
-          <h5 className="card-title">{universidad.nombre_universidad}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">
-            Universidad de Interés
-          </h6>
-          <p className="card-text">
-            <a
-              href={universidad.maps_universidad}
-              target="_blank"
-              rel="noreferrer"
+      {universidad && (
+        <div className="card w-full h-full">
+          <div className="card-body">
+            <h5 className="card-title">{universidad.nombre_universidad}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              Universidad de Interés
+            </h6>
+            <p className="card-text">
+              <a
+                href={universidad.maps_universidad}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {universidad.direccion_universidad},{" "}
+                {universidad.localidad_universidad},{" "}
+                {universidad.zona_universidad}
+              </a>
+            </p>
+            <br />
+            <button
+              onClick={() => {
+                setIdUniToShowInfo(id_universidad);
+                setButtonPopUpVerMas(true);
+              }}
             >
-              {universidad.direccion_universidad},{" "}
-              {universidad.localidad_universidad},{" "}
-              {universidad.zona_universidad}
-            </a>
-          </p>
-          <br />
-          <button
-            onClick={() => {
-              setIdUniToShowInfo(id_universidad);
-              setButtonPopUpVerMas(true);
-            }}
-          >
-            Ver información completa de la universidad
-          </button>
-          <br />
-          <br />
-          <a href="#" className="card-link">
-            Ver en el mapa
-          </a>
-          <br />
-          <button
-            onClick={() => {
-              setIdUniToShowInfo(id_universidad);
-              setButtonPopUpCarta(true);
-            }}
-          >
-            Enviar Carta
-          </button>
-          <h2 className="right-0 inline pl-40">Estado: </h2>
-          <p className="inline text-green-500">ACEPTADO</p>
+              Ver información completa de la universidad
+            </button>
+            <br />
+            <br />
+            <Link
+              to={`/mapa/@${universidad.Point.x},${universidad.Point.y},13z?selected=${id_universidad}`}
+              className="card-link"
+            >
+              Ver en el mapa
+            </Link>
+            <br />
+            <button
+              onClick={() => {
+                setIdUniToShowInfo(id_universidad);
+                setButtonPopUpCarta(true);
+              }}
+            >
+              Enviar Carta
+            </button>
+            <h2 className="right-0 inline pl-40">Estado: </h2>
+            <p className="inline text-green-500">ACEPTADO</p>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
