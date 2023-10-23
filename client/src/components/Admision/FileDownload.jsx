a447e4b846805cc03a74d29ee238302d2e69f56c
MichatNovato <86722428+Tiago204@users.noreply.github.com>
2023-10-23 14:47:20 -0300

    Descargar Carta

---
+++ client/src/components/Admision/FileDownload.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { get } from "../../api/api";
+import {useOutletContext} from "react-router-dom"
+
+function FileDownload({ id_universidad }) {
+  const { token } = useOutletContext();
+  const [downloadedFile, setDownloadedFile] = useState("");
+
+  const downloadFile = async () => {
+    const res = await axios.get(
+      `http://localhost:4000/api/download?idUniversidad=${id_universidad}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        responseType: 'blob',
+      }
+    );
+
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'downloaded-file.txt');
+    document.body.appendChild(link);
+    link.click();
+    window.URL.revokeObjectURL(url);
+  }
+  return (<button onClick={downloadFile}>Descargar Carta</button>);
+};
+
+export default FileDownload;
